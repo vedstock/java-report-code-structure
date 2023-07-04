fcf8b9d88a095e69efb7f7cca8dca1f079e92994
Chuong Tran <40161611+vedstock@users.noreply.github.com>
2023-07-04 23:35:31 +0700

    Create GradeUserServiceImpl.java

---
+++ General_Report_System/src/main/java/com/phl/general/report/service/impl/GradeUserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.phl.general.report.service.impl;
+
+import com.phl.common.dto.ItemResult;
+import com.phl.common.dto.RequestGWDto;
+import com.phl.common.dto.ResponseEntityDto;
+import com.phl.common.exception.BusException;
+import com.phl.general.report.common.dto.GradeUserDto;
+import com.phl.general.report.service.GradeUserService;
+
+public class GradeUserServiceImpl implements GradeUserService {
+	public ItemResult viewGradeUserCourse(GradeUserDto gradeUserDto)	throws BusException, Exception {
+		ItemResult itemResult = null;
+		return null;
+	}
+}
