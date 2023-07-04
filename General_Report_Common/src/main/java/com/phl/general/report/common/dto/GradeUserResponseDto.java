f7d5ddbec30578a9503191801237c4b5f11d4ade
Chuong Tran <40161611+vedstock@users.noreply.github.com>
2023-07-04 10:03:55 +0700

    Create GradeUserResponseDto.java

---
+++ General_Report_Common/src/main/java/com/phl/general/report/common/dto/GradeUserResponseDto.java
@@ -0,0 +1,13 @@
+package com.phl.general.report.common.dto;
+
+public class GradeUserResponseDto {
+	String base64;
+
+	public String getBase64() {
+		return base64;
+	}
+
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+}
