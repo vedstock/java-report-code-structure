20658edebd1b369313bfa555005989ad5b489272
Chuong Tran <40161611+vedstock@users.noreply.github.com>
2023-07-04 10:04:22 +0700

    Create GradeUserRequestDto.java

---
+++ General_Report_Common/src/main/java/com/phl/general/report/common/dto/GradeUserRequestDto.java
@@ -0,0 +1,11 @@
+package com.phl.general.report.common.dto;
+
+public class GradeUserRequestDto {
+	GradeUserDto gradeUserDto;
+	public GradeUserDto getGradeUserDto() {
+		return gradeUserDto;
+	}
+	public void setGradeUserDto(GradeUserDto gradeUserDto) {
+		this.gradeUserDto = gradeUserDto;
+	}
+}
