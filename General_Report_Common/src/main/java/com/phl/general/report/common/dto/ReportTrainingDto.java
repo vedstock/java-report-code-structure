51240fcc73d32c3f2cd576b3feb0ffef561ea051
Chuong Tran <40161611+vedstock@users.noreply.github.com>
2023-07-25 09:12:56 +0700

    Create ReportTrainingDto.java

---
+++ General_Report_Common/src/main/java/com/phl/general/report/common/dto/ReportTrainingDto.java
@@ -0,0 +1,337 @@
+package com.phl.general.report.common.dto;
+
+public class ReportTrainingDto {
+	Integer id;
+	String courseIdnum;		// A
+	String cohortIdnum;		// B
+	String cohortStart;		// C
+	String cohortEnd;		// D
+	String examIdnum;		// E
+	String examDate;		// F
+	String state;			// G
+	String userFirstname;	// H1
+	String userLastname;	// H2
+	String userFullname;
+	String birthDate;		// I
+	String monthDate;		// J
+	String yearDate;		// K
+	String sex;				// L
+	String userIdnum;		// M
+	String agentCode;		// N
+	String trainer;			// O
+	String supervisor;		// P
+	String grade;			// Q
+	String office;			// R
+	String area;			// S
+	String tdInfo;			// T
+	String tdName;			
+	String rdInfo;			
+	String rdName;			// U
+	String adInfo;			
+	String adName;			// V
+	String currentResult;	// W
+	String reserveResult;	// X
+	String date1;			// Y
+	String date2;			// Z
+	String date3;			// AA
+	String date4;			// AB
+	String date5;			// AC
+	String date6;			// AD
+	String qualified;		// AE
+	String mofGrade;		// AF
+	String passedQuantity;	// AG
+	String online;			// AH
+	/* params */
+	String paramCohort;
+	String paramExam;
+	String paramCategory;
+	String paramFrom;
+	String paramTo;
+	String paramUsername;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCourseIdnum() {
+		return courseIdnum;
+	}
+	public void setCourseIdnum(String courseIdnum) {
+		this.courseIdnum = courseIdnum ;
+	}
+	public String getCohortIdnum() {
+		return cohortIdnum;
+	}
+	public void setCohortIdnum(String cohortIdnum) {
+		this.cohortIdnum = cohortIdnum ;
+	}
+	public String getCohortStart() {
+		return cohortStart;
+	}
+	public void setCohortStart(String cohortStart) {
+		this.cohortStart = cohortStart ;
+	}
+	public String getCohortEnd() {
+		return cohortEnd;
+	}
+	public void setCohortEnd(String cohortEnd) {
+		this.cohortEnd = cohortEnd ;
+	}
+
+	public String getExamIdnum() {
+		return examIdnum;
+	}
+	public void setExamIdnum(String examIdnum) {
+		this.examIdnum = examIdnum ;
+	}
+	public String getExamDate() {
+		return examDate;
+	}
+	public void setExamDate(String examDate) {
+		this.examDate = examDate ;
+	}
+
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state ;
+	}
+	public String getUserFirstname() {
+		return userFirstname;
+	}
+	public void setUserFirstname(String userFirstname) {
+		this.userFirstname = userFirstname ;
+	}
+
+	public String getUserLastname() {
+		return userLastname;
+	}
+	public void setUserLastname(String userLastname) {
+		this.userLastname = userLastname ;
+	}
+	public String getUserFullname() {
+		return userFullname;
+	}
+	public void setUserFullname(String userFullname) {
+		this.userFullname = userFullname ;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate ;
+	}
+
+	public String getMonthDate() {
+		return monthDate;
+	}
+	public void setMonthDate(String monthDate) {
+		this.monthDate = monthDate ;
+	}
+	public String getYearDate() {
+		return yearDate;
+	}
+	public void setYearDate(String yearDate) {
+		this.yearDate = yearDate ;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex ;
+	}
+	public String getUserIdnum() {
+		return userIdnum;
+	}
+	public void setUserIdnum(String userIdnum) {
+		this.userIdnum = userIdnum ;
+	}
+	public String getAgentCode() {
+		return agentCode;
+	}
+	public void setAgentCode(String agentCode) {
+		this.agentCode = agentCode ;
+	}
+	public String getTrainer() {
+		return trainer;
+	}
+	public void setTrainer(String trainer) {
+		this.trainer = trainer ;
+	}
+	public String getSupervisor() {
+		return supervisor;
+	}
+	public void setSupervisor(String supervisor) {
+		this.supervisor = supervisor ;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade ;
+	}
+
+	public String getOffice() {
+		return office;
+	}
+	public void setOffice(String office) {
+		this.office = office ;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area ;
+	}
+
+	public String getTdInfo() {
+		return tdInfo;
+	}
+	public void setTdInfo(String tdInfo) {
+		this.tdInfo = tdInfo ;
+	}
+	public String getTdName() {
+		return tdName;
+	}
+	public void setTdName(String tdName) {
+		this.tdName = tdName ;
+	}
+	public String getRdInfo() {
+		return rdInfo;
+	}
+	public void setRdInfo(String rdInfo) {
+		this.rdInfo = rdInfo ;
+	}
+	public String getRdName() {
+		return rdName;
+	}
+	public void setRdName(String rdName) {
+		this.rdName = rdName ;
+	}
+
+	public String getAdInfo() {
+		return adInfo;
+	}
+	public void setAdInfo(String adInfo) {
+		this.adInfo = adInfo ;
+	}
+	public String getAdName() {
+		return adName;
+	}
+	public void setAdName(String adName) {
+		this.adName = adName ;
+	}
+
+	public String getCurrentResult() {
+		return currentResult;
+	}
+	public void setCurrentResult(String currentResult) {
+		this.currentResult = currentResult ;
+	}
+	public String getReserveResult() {
+		return reserveResult;
+	}
+	public void setReserveResult(String reserveResult) {
+		this.reserveResult = reserveResult ;
+	}
+	public String getDate1() {
+		return date1;
+	}
+	public void setDate1(String date1) {
+		this.date1 = date1 ;
+	}
+	public String getDate2() {
+		return date2;
+	}
+	public void setDate2(String date2) {
+		this.date2 = date2 ;
+	}
+	public String getDate3() {
+		return date3;
+	}
+	public void setDate3(String date3) {
+		this.date3 = date3 ;
+	}
+	public String getDate4() {
+		return date4;
+	}
+	public void setDate4(String date4) {
+		this.date4 = date4 ;
+	}
+	public String getDate5() {
+		return date5;
+	}
+	public void setDate5(String date5) {
+		this.date5 = date5 ;
+	}
+	public String getDate6() {
+		return date6;
+	}
+	public void setDate6(String date6) {
+		this.date6 = date6 ;
+	}
+	public String getQualified() {
+		return qualified;
+	}
+	public void setQualified(String qualified) {
+		this.qualified = qualified ;
+	}
+	public String getMofGrade() {
+		return mofGrade;
+	}
+	public void setMofGrade(String mofGrade) {
+		this.mofGrade = mofGrade ;
+	}
+	public String getPassedQuantity() {
+		return passedQuantity;
+	}
+	public void setPassedQuantity(String passedQuantity) {
+		this.passedQuantity = passedQuantity ;
+	}
+	public String getOnline() {
+		return online;
+	}
+	public void setOnline(String online) {
+		this.online = online ;
+	}
+	// Get Set Params
+	public String getParamCohort() {
+		return paramCohort;
+	}
+	public void setParamCohort(String paramCohort) {
+		this.paramCohort = paramCohort;
+	}
+	public String getParamExam() {
+		return paramExam;
+	}
+	public void setParamExam(String paramExam) {
+		this.paramExam = paramExam;
+	}
+	public String getParamFrom() {
+		return paramFrom;
+	}
+	public void setParamFrom(String paramFrom) {
+		this.paramFrom = paramFrom;
+	}
+	public String getParamTo() {
+		return paramTo;
+	}
+	public void setParamTo(String paramTo) {
+		this.paramTo = paramTo;
+	}
+	public String getParamCategory() {
+		return paramCategory;
+	}
+	public void setParamCategory(String paramCategory) {
+		this.paramCategory = paramCategory;
+	}
+	public String getParamUsername() {
+		return paramUsername;
+	}
+	public void setParamUsername(String paramUsername) {
+		this.paramUsername = paramUsername;
+	}
+}
